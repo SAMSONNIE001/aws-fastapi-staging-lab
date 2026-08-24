ed9ebd8700c3eb8cd35ba06ba6fc0adaaa7a9efd
samuel sunny eke <sunnyekesamuel@gmail.com>
2026-08-24 17:39:18 +0100

    Use environment variable for health check response

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
+
+APP_ENV = os.getenv("APP_ENV", "development")
 
 @app.get("/")
 def home():
@@ -8,4 +11,7 @@ def home():
 
 @app.get("/health")
 def health():
-    return {"status": "ok", "environment": "staging"}
+    return {
+        "status": "ok",
+        "environment": APP_ENV
+    }
